332831a2af6fe905f57e83256dfebeb965974ff0
Erik Salas <25646533+eriksalascode@users.noreply.github.com>
2021-08-09 19:48:04 -0500

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Erik Salas on 8/9/21.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
