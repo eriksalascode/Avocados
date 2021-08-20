79abdf5ae4efb3b1b8747afc2264f719ceb1781f
Erik Salas <25646533+eriksalascode@users.noreply.github.com>
2021-08-20 08:31:15 -0500

    Design header view, add animation, insert header to content view

--- Avocados/View/ContentView.swift
+++ Avocados/View/ContentView.swift
@@ -9,8 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Avocado Recipes")
-            .padding()
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20) {
+                //MARK: Header
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(alignment: .top, spacing: 0) {
+                        HeaderView()
+                    }
+                }
+                
+                //MARK: Footer
+                VStack(alignment: .center, spacing: 20) {
+                    Text("All About Avocados")
+                        .font(.system(.title, design: .serif))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("ColorGreenAdaptive"))
+                        .padding(8)
+                    Text("Everything you wanted to know about avocados but were too afraid to ask.")
+                        .font(.system(.body, design: .serif))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color.gray)
+                }
+                .frame(maxWidth: 640)
+                .padding()
+                .padding(.bottom, 85)
+            }
+        } //:SCROLLVIEW
+        .edgesIgnoringSafeArea(.all)
+        .padding(0)
     }
 }
 
